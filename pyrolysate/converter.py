ec4b971dadcfbd79212dba004b9352378cd9da86
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2025-04-30 07:49:54 -0400

    Run class instance
    Original method didn't correctly bind self to function which would lead to "missing one positional argument" error
    
    Updated code allows wrapper to copy any dunder methods from the original function as well as support class instances (that of which is used in the tests)

--- pyrolysate/converter.py
+++ pyrolysate/converter.py
@@ -1,8 +1,9 @@
 # Function decorators and caching
-from functools import cache
+from functools import cache, update_wrapper
 
 # Async Support
 import asyncio
+import types
 
 # Data formats and compression
 import bz2
@@ -35,13 +36,21 @@ def main():
 class AsyncWrapper:
     def __init__(self, func):
         self._func = func
-        self.__name__ = getattr(func, "__name__", "AsyncWrapper")
+        self._is_coroutine = inspect.iscoroutinefunction(func)
+        update_wrapper(self, func) # Copy dunder metadata from original function
 
     def __call__(self, *args, **kwargs):
         return self._func(*args, **kwargs)
 
     async def run_async(self, *args, **kwargs):
-        return await asyncio.to_thread(self._func, *args, **kwargs)
+        if self._is_coroutine:
+            return await self._func(*args, **kwargs) # Awaits already async function
+        else:
+            return await asyncio.to_thread(self._func, *args, **kwargs) # Sends sync function to thread
+
+    def __get__(self, instance, owner):
+        # Support instance methods (bind 'self')
+        return types.MethodType(self, instance)
 
 def async_support(func):
     return AsyncWrapper(func)
