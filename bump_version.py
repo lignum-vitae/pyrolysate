f4428b2a9b428e86a3bc8e3a3d0a1b5b8d8afeb2
Andrew Hennis <andrew.mr.hennis@gmail.com>
2026-03-13 13:44:12 +0000

    update dependencies and format project

--- bump_version.py
+++ bump_version.py
@@ -9,15 +9,15 @@ from pathlib import Path
 root_dir = Path(__file__).parent
 pyproject_file = root_dir / "pyproject.toml"
 
-with open(pyproject_file, 'rb') as f:
+with open(pyproject_file, "rb") as f:
     toml_file = tomllib.load(f)
 
 project_version = toml_file["project"]["version"]
-(major, minor, patch) = [int(x) for x in project_version.split('.')]
-patch += 1 # bump just the patch version
+(major, minor, patch) = [int(x) for x in project_version.split(".")]
+patch += 1  # bump just the patch version
 
 bumped_project_version = ".".join([str(x) for x in [major, minor, patch]])
 toml_file["project"]["version"] = bumped_project_version
 
-with open(pyproject_file, 'w') as f:
+with open(pyproject_file, "w") as f:
     toml.dump(toml_file, f)
